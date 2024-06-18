4d5514689b8fa87080ecfaf32d9ee970252eb068
Abdulsamad Abdikadir Osman <145560781+Abdulsamad-prog@users.noreply.github.com>
2024-06-18 05:16:45 +0300

    Create about us.php

---
+++ about us.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Sparkle Cleaners</title>
+    <link rel="stylesheet" href="Style.CSS">
+    <style>
+        body
+         { 
+            background-image:url("https://th.bing.com/th/id/OIP.5VK49mEbeokKnTqN02_ixgHaEK?rs=1&pid=ImgDetMain");
+            background-size: cover;
+            background-position: center;
+            height: 70vh; 
+        }
+        h1
+        {
+            color: purple ;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+    <header style="text-align: center;">
+        <h1>About Sparkle Cleaners</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="services.php">Our Services</a></li>
+            <li><a href="Contact us.php">Contact us</a></li>
+            <li><a href="Reviews.php">Reviews</a></li>
+            <li><a href="Logout.php">Logout</a></li>
+        </ul>
+    </nav>
+   
+    <section id="s1">
+        <h2 style="text-align: center;">Our Mission</h2>
+        <p>At Sparkle Cleaners, we are committed to providing unparalleled cleaning services to create a clean and healthy environment for our clients. Our dedicated team ensures attention to detail and customer satisfaction.Sparkle Cleaning Company was born out of a visionary idea and a passion for creating pristine spaces. The brainchild of its founder, Emily Thompson, Sparkle originated from her desire to provide high-quality cleaning services that not only met but exceeded the expectations of clients. Emily, with a background in hospitality and a keen eye for detail, recognized the need for a cleaning company that prioritized professionalism, reliability, and a commitment to spotless environments. Fueled by her entrepreneurial spirit, Emily gathered a team of dedicated professionals who shared her vision of transforming spaces into gleaming showcases. Today, Sparkle Cleaning Company stands as a testament to Emily's vision, consistently delivering exceptional cleaning services that bring a touch of brilliance to homes and businesses alike.</p>
+        <p><i>Contact us today for a sparkling experience!</i></p>
+    </section>
+
+    <ul>
+        <li>Residential Cleaning</li>
+        <li>Commercial Cleaning</li>
+        <li>Carpet Cleaning</li>
+        <li>Window Cleaning</li>
+    </ul>
+
+<img src="https://www.shutterstock.com/image-photo/brushes-bottles-cleaning-liquids-sponges-260nw-2246148707.jpg" width="30%">
+    <p>For more information, visit our <a href="index.html">Home</a> page.</p>
+
+</body>
+</html>
